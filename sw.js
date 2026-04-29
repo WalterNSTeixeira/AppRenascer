4318c11ba2c581cfa500d7afa825fc13036cebfb
WalterNSTeixeira <walternsteixeira@gmail.com>
2026-04-29 15:31:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', function(event) {});
+const cacheName = 'renascer-v1';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
